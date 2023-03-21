8eb377e7ae8929ecff00e408a16ad6e50e62fb22
Matthew Kang <matthew.kang@teladoc.com>
2023-03-21 14:52:39 -0700

    environment variables impl

--- backend/python/src/ccc_audit_call_lambda/run.py
+++ backend/python/src/ccc_audit_call_lambda/run.py
@@ -2,18 +2,21 @@ import os
 import boto3
 import uuid
 import status_list
+import buckets
+import folders
 from datetime import datetime
 
 s3 = boto3.resource("s3")
 
 
 def lambda_handler(event, context):
-    print("Event Name ", event)
+    print("Event: ", event)
 
     # Get bucket and file name
     bucket = event['detail']['bucket']['name']
     key = event['detail']['object']['key']
 
+    print("Bucket: ", bucket)
 
     folder = os.path.dirname(key)
     file = os.path.basename(key)
@@ -41,27 +44,28 @@ def lambda_handler(event, context):
 
     status = ""
 
-    if path == "customercallcenterpiiunrefined":
-        status = status_list.AUDIO_FILE_RECEIVED
-    elif path == "customercallcenterpiitranscription/standard/":
-        status = status_list.AUDIO_TRANSCRIPT_WITHSPEAKERLABEL_GENERATED
-    elif path == "customercallcenterpiitranscription/standard_full_transcripts/":
-        status = status_list.TRANSCRIPT_SIMPLETEXT_WITHPII_GENERATED
-    elif path == "customercallcenterpiicleaned/standard_full_transcripts/":
-        status = status_list.TRANSCRIPT_SIMPLETEXT_NOPII_GENERATED
-    elif path == "customercallcenterpiicleaned/final_outputs/":
-        status = status_list.FINAL_INDENTED_OUTPUT_GENERATED
-    elif path == "customercallcenterpiicleaned/final_outputs2/":
-        status = status_list.FINAL_NONINDENTED_OUTPUT_GENERATED
-    elif path == "customercallcenterpiicleanedverified/standard_full_transcripts/":
-        status = status_list.STANDARD_FULL_TRANSCRIPT_CLEANED_VERIFIED
-    elif path == "customercallcenterpiicleanedverified/final_outputs/":
-        status = status_list.FINAL_VERIFIED_INDENTED_OUTPUT_GENERATED
-    elif path == "customercallcenterpiicleanedverified/final_outputs2/":
-        status = status_list.FINAL_VERIFIED_NONINDENTED_OUTPUT_GENERATED
-    elif path == "customercallcenterpiidirty/":
-        status = status_list.STANDARD_FULL_TRANSCRIPT_DIRTY_VERIFIED
+    print("Path: ", path)
 
+    if path == buckets.UNREFINED:
+        status = status_list.AUDIO_FILE_RECEIVED
+    elif path == buckets.TRANSCRIPTION + "/" + folders.STANDARD + "/":
+        status = status_list.AUDIO_TRANSCRIPT_WITHSPEAKERLABEL_GENERATED
+    elif path == buckets.TRANSCRIPTION + "/" + folders.STANDARD_FULL_TRANSCRIPTS + "/":
+        status = status_list.TRANSCRIPT_SIMPLETEXT_WITHPII_GENERATED
+    elif path == buckets.CLEANED + "/" + folders.STANDARD_FULL_TRANSCRIPTS + "/":
+        status = status_list.TRANSCRIPT_SIMPLETEXT_NOPII_GENERATED
+    elif path == buckets.CLEANED + "/" + folders.FINAL_OUTPUTS + "/":
+        status = status_list.FINAL_INDENTED_OUTPUT_GENERATED
+    elif path == buckets.CLEANED + "/" + folders.FINAL_OUTPUTS2 + "/":
+        status = status_list.FINAL_NONINDENTED_OUTPUT_GENERATED
+    elif path == buckets.CLEANED_VERIFIED + "/" + folders.STANDARD_FULL_TRANSCRIPTS + "/":
+        status = status_list.STANDARD_FULL_TRANSCRIPT_CLEANED_VERIFIED
+    elif path == buckets.CLEANED_VERIFIED + "/" + folders.FINAL_OUTPUTS + "/":
+        status = status_list.FINAL_VERIFIED_INDENTED_OUTPUT_GENERATED
+    elif path == buckets.CLEANED_VERIFIED + "/" + folders.FINAL_OUTPUTS2 + "/":
+        status = status_list.FINAL_VERIFIED_NONINDENTED_OUTPUT_GENERATED
+    elif path == buckets.DIRTY:
+        status = status_list.STANDARD_FULL_TRANSCRIPT_DIRTY_VERIFIED
 
     now = datetime.now()
     # dd/mm/YY H:M:S
@@ -70,9 +74,3 @@ def lambda_handler(event, context):
     table.put_item(
         Item={"UUID": id, "CallType": call_type, "CallID": call_id, "Path": path, "FileName": file, "Status": status,
               "DateTimeStamp": dt_string})
-
-
-if __name__ == '__main__':
-    os.environ["TABLE_NAME"] = "visit-count-table"
-    event = {"user": "jack"}
-    print(lambda_handler(event, None))
