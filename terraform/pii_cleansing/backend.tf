e80a8891eb7aede34e46054470354e64599e0cd0
CShows <CShows@sdgecontractor.com>
2023-03-22 10:01:58 -0700

    add pipeline workspace

--- terraform/pii_cleansing/backend.tf
+++ terraform/pii_cleansing/backend.tf
@@ -1,11 +1,10 @@
 terraform {
   backend "remote" {
     hostname     = "app.terraform.io"
-   organization = "SempraUtilities"
- 
-   workspaces {
-      # name = "#{terraformWorkspace}#"
-      name = "sdge-dev_sdge-it-nla"
+    organization = "SempraUtilities"
+
+    workspaces {
+      name = "#{terraformWorkspace}#"
     }
   }
 }
