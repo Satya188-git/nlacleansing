1992164ecd6980ecfe8deaa20b2c964964b94530
CShows <CShows@sdgecontractor.com>
2023-03-31 17:48:05 -0700

    remove jsonencoding

--- terraform/pii_cleansing/modules/s3/main.tf
+++ terraform/pii_cleansing/modules/s3/main.tf
@@ -401,7 +401,7 @@ module "ccc_maciefindings_bucket" {
       expose_headers  = []
     }
   ]
-  additional_policy_statements = [jsonencode([
+  additional_policy_statements = [[
     {
       "Sid"       = "AllowSSLRequestsOnly",
       "Effect"    = "Deny",
@@ -457,7 +457,7 @@ module "ccc_maciefindings_bucket" {
         }
       }
     }
-  ])]
+  ]]
 }
 
 module "ccc_piimetadata_bucket" {
