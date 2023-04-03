73caeb1c1ca0563ac079151559499d27017cfa3d
Rajrup Sil <rajrup.sil@cox.com>
2023-04-03 17:05:37 +0530

    Call duration dropdown changes

--- frontend/components/CCC/TimeSelector.tsx
+++ frontend/components/CCC/TimeSelector.tsx
@@ -1,5 +1,6 @@
 import { CaretDownOutlined, CaretUpOutlined } from '@ant-design/icons';
 import { Select, Typography } from 'antd';
+import { callDurations } from 'constants/callDuraions';
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { setCallDuration } from 'reducers/ccc-reducer';
@@ -11,31 +12,6 @@ const TimeSelector: React.FC = () => {
 	const dispatch = useDispatch();
 	const callDuration = useSelector((state: any) => state.ccc.callDuration);
 
-	const handleChange = (value: string) => {
-		let time = '';
-		switch (value) {
-			case '5':
-				time = '<5MINS';
-				break;
-			case '10':
-				time = '<10MINS';
-				break;
-			case '20':
-				time = '<20MINS';
-				break;
-			case '30':
-				time = '<30MINS';
-				break;
-			case '45':
-				time = '<45MINS';
-				break;
-			case '+':
-				time = '>45MINS';
-				break;
-		}
-		dispatch(setCallDuration(time));
-	};
-
 	return (
 		<>
 			<Title level={5}>Call Duration</Title>
@@ -46,30 +22,16 @@ const TimeSelector: React.FC = () => {
 				suffixIcon={open ? <CaretUpOutlined /> : <CaretDownOutlined />}
 				style={{ width: '100%' }}
 				value={callDuration}
-				onChange={handleChange}
+				onChange={(value) => dispatch(setCallDuration(value))}
 				onDropdownVisibleChange={(open) => setOpen(open)}
 			>
-				<Option key='all' value=''>
-					All Call Duration
-				</Option>
-				<Option key='5' value='5'>
-					&lt; 5 MINS
-				</Option>
-				<Option key='10' value='10'>
-					&lt; 10 MINS
-				</Option>
-				<Option key='20' value='20'>
-					&lt; 20 MINS
-				</Option>
-				<Option key='30' value='30'>
-					&lt; 30 MINS
-				</Option>
-				<Option key='45' value='45'>
-					&lt; 45 MINS
-				</Option>
-				<Option key='+' value='+'>
-					&gt; 45 MINS
-				</Option>
+				{
+					callDurations.map(d => (
+						<Option key={d.value} value={d.value}>
+							{d.name}
+						</Option>
+					))
+				}
 			</Select>
 		</>
 	);
