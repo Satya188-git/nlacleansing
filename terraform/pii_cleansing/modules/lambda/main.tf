81668d831ee1175e6961052b2f00221142ade0d4
Cristian Nita <cristian.nita@accenture.com>
2023-04-05 12:24:59 +0300

    adding missing lambda env vars

--- terraform/pii_cleansing/modules/lambda/main.tf
+++ terraform/pii_cleansing/modules/lambda/main.tf
@@ -58,6 +58,16 @@ module "ccc_transcribe_lambda" {
     CONF_DESTINATION_BUCKET_NAME = var.ccc_initial_bucket_id
     CONF_S3BUCKET_OUTPUT         = var.ccc_unrefined_call_data_bucket_id
     CONF_ROLE_ARN                = var.custom_transcribe_lambda_role_arn
+    CONF_CUSTOM_VOCAB_NAME       = "CCC-CustomVocabs"
+    CONF_FILTER_NAME	           = "TestVocabFilter1"
+    CONF_MAX_SPEAKERS	           = "2"
+    CONF_REDACTION_LANGS	       = "en-US"
+    CONF_REDACTION_TRANSCRIPT	   = "true"
+    CONF_SPEAKER_MODE	           = "channel"
+    CONF_TRANSCRIBE_API	         = "analytics"
+    CONF_TRANSCRIBE_LANG	       = "en-US"
+    CONF_VOCABNAME               = "undefined"
+    CONF_VOCAB_FILTER_MODE	     = "mask"
   }
 
   tags = merge(local.tags,
@@ -147,6 +157,8 @@ module "ccc_informational_macie_lambda" {
   environment_variables = {
     DESTINATION_BUCKET_NAME_VERIFIED = var.ccc_verified_clean_bucket_id
     DESTINATION_BUCKET_NAME_DIRTY    = var.ccc_dirty_bucket_id
+    TRANSCRIPTION_BUCKET_NAME        = var.ccc_initial_bucket_id
+    UNREFINED_BUCKET_NAME            = var.ccc_unrefined_call_data_bucket_id
   }
 
   s3_existing_package = {
