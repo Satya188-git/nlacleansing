bc5e9b7a7ee72d6d502c8a0127b8105f2c7c8c59
Cristian Nita <cristian.nita@accenture.com>
2023-04-13 15:24:17 +0300

    assumed role changes

--- terraform/pii_cleansing/provider.tf
+++ terraform/pii_cleansing/provider.tf
@@ -2,7 +2,7 @@ provider "aws" {
   #alias = "nla-pii"
   region = var.region
   assume_role {
-    role_arn     = var.aws_assume_role_pii
+    role_arn     = var.aws_assume_role
     session_name = "AWS-STSSession-PII"
   }
 }
