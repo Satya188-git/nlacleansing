017df1d58fb179a16b58daf1b2fba3c8cdfddd72
Cristian Nita <cristian.nita@accenture.com>
2023-04-18 12:49:56 +0300

    correct terraform workspace

--- terraform/pii_cleansing/terraform.tf
+++ terraform/pii_cleansing/terraform.tf
@@ -1,20 +1,10 @@
-# terraform {
-#   backend "remote" {
-#     hostname     = "#{ terraformHostname }#"
-#     organization = "#{ System.TeamProject }#"
-
-#     workspaces {
-#       name = "#{ terraformWorkspace }#"
-#     }
-#   }
-# }
 terraform {
   backend "remote" {
-    hostname     = "app.terraform.io"
-   organization = "SempraUtilities"
- 
-   workspaces {
-      name = "sdge-qa_sdge-it-nla"
+    hostname     = "#{ terraformHostname }#"
+    organization = "#{ System.TeamProject }#"
+
+    workspaces {
+      name = "#{ terraformWorkspace }#"
     }
   }
 }
