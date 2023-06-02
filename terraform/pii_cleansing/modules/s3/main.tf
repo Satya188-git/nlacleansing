e254c9315c54df83a6ae1d704ab75385654706fa
Cristian Nita <CNita@scgcontractor.com>
2023-06-02 10:56:34 +0000

    Merged PR 76290: [DevOps] Merge develop to master for QA deployment
    [DevOps] Merge develop to master for QA deployment

--- terraform/pii_cleansing/modules/s3/main.tf
+++ terraform/pii_cleansing/modules/s3/main.tf
@@ -41,25 +41,20 @@ module "ccc_unrefined_call_data_bucket" {
         # kms_master_key_id = "alias/aws/kms" # revert to aws s3 managed key after provider bug workaround 
         kms_master_key_id = var.kms_key_ccc_unrefined_arn
         sse_algorithm     = "aws:kms"
-        # sse_algorithm       = "AES256"
       }
     }
   }
-  # lifecycle_rule = [{
-  #   id      = "expiration-after-60-days"
-  #   enabled = true
-  #   expiration = [
-  #     {
-  #       days = 60
-  #     },
-  #   ]
-  # }]
+  lifecycle_rule = [{
+    id      = "expiration-rule"
+    enabled = true
+    expiration = [
+      {
+        days = 2192
+      },
+    ]
+  }]
 }
 
-resource "aws_s3_bucket_notification" "unrefined_call_data_bucket_notification" {
-  bucket      = module.ccc_unrefined_call_data_bucket.s3_bucket_id
-  eventbridge = true
-}
 
 module "ccc_initial_bucket" {
   source                         = "app.terraform.io/SempraUtilities/seu-s3/aws"
@@ -83,25 +78,21 @@ module "ccc_initial_bucket" {
         # kms_master_key_id = "alias/aws/kms" # revert to aws s3 managed key after provider bug workaround
         kms_master_key_id = var.kms_key_ccc_initial_arn
         sse_algorithm     = "aws:kms"
-        # sse_algorithm       = "AES256"
       }
     }
   }
-  # lifecycle_rule = [{
-  #   id      = "expiration-after-60-days"
-  #   enabled = true
-  #   expiration = [
-  #     {
-  #       days = 60
-  #     },
-  #   ]
-  # }]
+  lifecycle_rule = [{
+    id      = "expiration-rule"
+    enabled = true
+    expiration = [
+      {
+        days = 2192
+      },
+    ]
+  }]
 }
 
-resource "aws_s3_bucket_notification" "ccc_initial_bucket_notification" {
-  bucket      = module.ccc_initial_bucket.s3_bucket_id
-  eventbridge = true
-}
+
 module "ccc_cleaned_bucket" {
   source                         = "app.terraform.io/SempraUtilities/seu-s3/aws"
   version                        = "5.3.2"
@@ -127,21 +118,17 @@ module "ccc_cleaned_bucket" {
       }
     }
   }
-  # lifecycle_rule = [{
-  #   id      = "expiration-after-60-days"
-  #   enabled = true
-  #   expiration = [
-  #     {
-  #       days = 60
-  #     },
-  #   ]
-  # }]
+  lifecycle_rule = [{
+    id      = "expiration-rule"
+    enabled = true
+    expiration = [
+      {
+        days = 2192
+      },
+    ]
+  }]
 }
 
-resource "aws_s3_bucket_notification" "ccc_cleaned_bucket_notification" {
-  bucket      = module.ccc_cleaned_bucket.s3_bucket_id
-  eventbridge = true
-}
 
 module "ccc_verified_clean_bucket" {
   source                         = "app.terraform.io/SempraUtilities/seu-s3/aws"
@@ -169,21 +156,17 @@ module "ccc_verified_clean_bucket" {
       }
     }
   }
-  # lifecycle_rule = [{
-  #   id      = "expiration-after-60-days"
-  #   enabled = true
-  #   expiration = [
-  #     {
-  #       days = 60
-  #     },
-  #   ]
-  # }]
+  lifecycle_rule = [{
+    id      = "expiration-rule"
+    enabled = true
+    expiration = [
+      {
+        days = 2192
+      },
+    ]
+  }]
 }
 
-resource "aws_s3_bucket_notification" "ccc_verified_clean_bucket_notification" {
-  bucket      = module.ccc_verified_clean_bucket.s3_bucket_id
-  eventbridge = true
-}
 
 module "ccc_dirty_bucket" {
   source                         = "app.terraform.io/SempraUtilities/seu-s3/aws"
@@ -211,21 +194,17 @@ module "ccc_dirty_bucket" {
       }
     }
   }
-  # lifecycle_rule = [{
-  #   id      = "expiration-after-60-days"
-  #   enabled = true
-  #   expiration = [
-  #     {
-  #       days = 60
-  #     },
-  #   ]
-  # }]
+  lifecycle_rule = [{
+    id      = "expiration-rule"
+    enabled = true
+    expiration = [
+      {
+        days = 2192
+      },
+    ]
+  }]
 }
 
-resource "aws_s3_bucket_notification" "ccc_dirty_bucket_notification" {
-  bucket      = module.ccc_dirty_bucket.s3_bucket_id
-  eventbridge = true
-}
 
 module "ccc_maciefindings_bucket" {
   source                         = "app.terraform.io/SempraUtilities/seu-s3/aws"
@@ -252,24 +231,15 @@ module "ccc_maciefindings_bucket" {
       }
     }
   }
-  # lifecycle_rule = [{
-  #   id      = "expiration-after-60-days"
-  #   enabled = true
-  #   expiration = [
-  #     {
-  #       days = 60
-  #     },
-  #   ]
-  # }]
-
-  # cors_rule = [
-  #   {
-  #     allowed_methods = ["GET", "PUT", "POST"]
-  #     allowed_origins = ["*"]
-  #     allowed_headers = ["*"]
-  #     expose_headers  = []
-  #   }
-  # ]
+  lifecycle_rule = [{
+    id      = "expiration-rule"
+    enabled = true
+    expiration = [
+      {
+        days = 2192
+      },
+    ]
+  }]
 
   additional_policy_statements = [
     {
@@ -315,10 +285,6 @@ module "ccc_maciefindings_bucket" {
   ]
 }
 
-resource "aws_s3_bucket_notification" "ccc_maciefindings_bucket_notification" {
-  bucket      = module.ccc_maciefindings_bucket.s3_bucket_id
-  eventbridge = true
-}
 
 module "ccc_piimetadata_bucket" {
   source                         = "app.terraform.io/SempraUtilities/seu-s3/aws"
@@ -345,21 +311,17 @@ module "ccc_piimetadata_bucket" {
       }
     }
   }
-  # lifecycle_rule = [{
-  #   id      = "expiration-after-60-days"
-  #   enabled = true
-  #   expiration = [
-  #     {
-  #       days = 60
-  #     },
-  #   ]
-  # }]
+  lifecycle_rule = [{
+    id      = "expiration-rule"
+    enabled = true
+    expiration = [
+      {
+        days = 2192
+      },
+    ]
+  }]
 }
 
-resource "aws_s3_bucket_notification" "ccc_piimetadata_bucket_notification" {
-  bucket      = module.ccc_piimetadata_bucket.s3_bucket_id
-  eventbridge = true
-}
 
 module "ccc_athenaresults_bucket" {
   source                         = "app.terraform.io/SempraUtilities/seu-s3/aws"
@@ -386,20 +348,51 @@ module "ccc_athenaresults_bucket" {
       }
     }
   }
-  # lifecycle_rule = [{
-  #   id      = "expiration-after-60-days"
-  #   enabled = true
-  #   expiration = [
-  #     {
-  #       days = 60
-  #     },
-  #   ]
-  # }]
+  lifecycle_rule = [{
+    id      = "expiration-rule"
+    enabled = true
+    expiration = [
+      {
+        days = 2192
+      },
+    ]
+  }]
 }
 
-resource "aws_s3_bucket_notification" "ccc_athenaresults_bucket_notification" {
-  bucket      = module.ccc_athenaresults_bucket.s3_bucket_id
-  eventbridge = true
+
+module "ccc_insights_audio_bucket" {
+  source  = "app.terraform.io/SempraUtilities/seu-s3/aws"
+  version = "5.3.2"
+
+  company_code     = local.company_code
+  application_code = local.application_code
+  environment_code = local.environment_code
+  region_code      = local.region_code
+  application_use  = "${local.application_use}-audio"
+  create_bucket    = true
+  force_destroy    = true
+  versioning       = true
+  tags             = local.tags
+  server_side_encryption_configuration = {
+    rule = {
+      bucket_key_enabled = true
+      apply_server_side_encryption_by_default = {
+        kms_master_key_id = var.kms_key_ccc_unrefined_arn
+        # kms_master_key_id = "alias/aws/s3" # revert to customer managed key after provider bug workaround
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+
+  lifecycle_rule = [{
+    id      = "expiration-rule"
+    enabled = true
+    expiration = [
+      {
+        days = 2192
+      },
+    ]
+  }]
 }
 
 
@@ -441,42 +434,6 @@ resource "aws_s3_bucket_replication_configuration" "insights_bucket_replication_
   }
 }
 
-module "ccc_insights_audio_bucket" {
-  source  = "app.terraform.io/SempraUtilities/seu-s3/aws"
-  version = "5.3.2"
-
-  company_code     = local.company_code
-  application_code = local.application_code
-  environment_code = local.environment_code
-  region_code      = local.region_code
-  application_use  = "${local.application_use}-audio"
-  create_bucket    = true
-  force_destroy    = true
-  versioning       = true
-  server_side_encryption_configuration = {
-    rule = {
-      bucket_key_enabled = true
-      apply_server_side_encryption_by_default = {
-        kms_master_key_id = var.kms_key_ccc_unrefined_arn
-        # kms_master_key_id = "alias/aws/s3" # revert to customer managed key after provider bug workaround
-        sse_algorithm     = "aws:kms"
-      }
-    }
-  }
-
-  # lifecycle_rule = [{
-  #   id      = "expiration-after-60-days"
-  #   enabled = true
-  #   expiration = [
-  #     {
-  #       days = 60
-  #     },
-  #   ]
-  # }]
-
-  tags = local.tags
-}
-
 
 resource "aws_s3_bucket_replication_configuration" "unrefined_bucket_replication_rule" {
   # Must have bucket versioning enabled first
@@ -509,136 +466,50 @@ resource "aws_s3_bucket_replication_configuration" "unrefined_bucket_replication
   }
 }
 
-# provider "aws" {
-#   alias  = "nla-insights"
-#   region = var.region
-#   assume_role {
-#     role_arn     = var.aws_assume_role_insights
-#     session_name = "AWS-STSSession-Insights"
-#   }
-# }
+
+resource "aws_s3_bucket_notification" "unrefined_call_data_bucket_notification" {
+  bucket      = module.ccc_unrefined_call_data_bucket.s3_bucket_id
+  eventbridge = true
+}
 
 
-# module "ccc_verified_clean_bucket_insights_account" {
-#   providers                      = { aws = aws.nla-insights }
-#   source                         = "app.terraform.io/SempraUtilities/seu-s3/aws"
-#   version                        = "5.3.2"
-#   company_code                   = local.company_code
-#   application_code               = local.application_code
-#   environment_code               = local.environment_code
-#   region_code                    = local.region_code
-#   application_use                = "${local.application_use}-verified-clean"
-#   owner                          = "NLA Team"
-#   create_bucket                  = true
-#   create_log_bucket              = false
-#   attach_alb_log_delivery_policy = false
-#   versioning                     = true
-#   tags                           = local.tags
-
-#   acl = "private"
-#   server_side_encryption_configuration = {
-#     rule = {
-#       apply_server_side_encryption_by_default = {
-#         kms_master_key_id = var.kms_key_ccc_verified_clean_insights_arn
-#         sse_algorithm     = "aws:kms"
-#       }
-#     }
-#   }
-# }
+resource "aws_s3_bucket_notification" "ccc_initial_bucket_notification" {
+  bucket      = module.ccc_initial_bucket.s3_bucket_id
+  eventbridge = true
+}
 
 
-
-# # source object ownership
-# resource "aws_s3_bucket_ownership_controls" "ccc_verified_clean_bucket" {
-#   bucket = module.ccc_verified_clean_bucket.s3_bucket_id
-#   rule {
-#     object_ownership = "BucketOwnerPreferred"
-#   }
-# }
-# # source bucket policy
-# data "aws_iam_policy_document" "insights_source_policy" {
-#   version = "2012-10-17"
-#   statement {
-#     actions = [
-#       "s3:ListBucket",
-#       "s3:GetReplicationConfiguration",
-#       "s3:GetObjectVersionForReplication",
-#       "s3:GetObjectVersionAcl",
-#       "s3:GetObjectVersionTagging",
-#       "s3:GetObjectVersion",
-#       "s3:ObjectOwnerOverrideToBucketOwner"
-#     ]
-#     effect = "Allow"
-#     resources = [
-#       "arn:aws:s3:::sdge-dtdes-dev-wus2-s3-nla-verified-clean",
-#       "arn:aws:s3:::sdge-dtdes-dev-wus2-s3-nla-verified-clean/*"
-#     ]
-#   }
-#   statement {
-#     actions = [
-#       "s3:ReplicateObject",
-#       "s3:ReplicateDelete",
-#       "s3:ReplicateTags",
-#       "s3:GetObjectVersionTagging",
-#       "s3:ObjectOwnerOverrideToBucketOwner"
-#     ]
-#     effect    = "Allow"
-#     resources = ["arn:aws:s3:::ccc-verified-cleaned-nla-temp/*"]
-#   }
-#   statement {
-#     actions = [
-#       "kms:Decrypt"
-#     ]
-#     effect    = "Allow"
-#     resources = ["arn:aws:kms:us-west-2:183095018968:key/551d47b3-97af-415b-ae31-71b6b7bc4cc0"]
-#   }
-#   statement {
-#     actions = [
-#       "kms:Encrypt"
-#     ]
-#     effect    = "Allow"
-#     resources = ["arn:aws:kms:us-west-2:713342716921:key/b71c79be-8406-4ade-8db7-6e68467f46e4"]
-#   }
-
-# }
-# resource "aws_s3_bucket_policy" "ccc_verified_clean_bucket" {
-#   bucket = module.ccc_verified_clean_bucket.s3_bucket_id
-#   policy = data.aws_iam_policy_document.insights_source_policy.json
-# }
+resource "aws_s3_bucket_notification" "ccc_cleaned_bucket_notification" {
+  bucket      = module.ccc_cleaned_bucket.s3_bucket_id
+  eventbridge = true
+}
 
 
-# # verified cllean replication bucket
-# module "insights_verified_clean_bucket" {
-#   source                         = "app.terraform.io/SempraUtilities/seu-s3/aws"
-#   version                        = "5.3.2"
-#   company_code                   = local.company_code
-#   application_code               = local.application_code
-#   environment_code               = local.environment_code
-#   region_code                    = local.region_code
-#   application_use                = "${local.application_use}-insights-verified-clean"
-#   owner                          = "NLA Team"
-#   create_bucket                  = true
-#   create_log_bucket              = false
-#   attach_alb_log_delivery_policy = false
-#   versioning                     = true
-#   tags                           = local.tags
+resource "aws_s3_bucket_notification" "ccc_verified_clean_bucket_notification" {
+  bucket      = module.ccc_verified_clean_bucket.s3_bucket_id
+  eventbridge = true
+}
 
-#   acl = "private"
-#   server_side_encryption_configuration = {
-#     rule = {
-#       apply_server_side_encryption_by_default = {
-#         kms_master_key_id = var.kms_key_ccc_verified_clean_arn
-#         sse_algorithm     = "aws:kms"
-#       }
-#     }
-#   }
-#   cors_rule = [
-#     {
-#       allowed_methods = ["GET", "PUT", "POST"]
-#       allowed_origins = ["*"]
-#       allowed_headers = ["*"]
-#       expose_headers  = []
-#     }
-#   ]
-# }
-# TODO add replication config
+
+resource "aws_s3_bucket_notification" "ccc_dirty_bucket_notification" {
+  bucket      = module.ccc_dirty_bucket.s3_bucket_id
+  eventbridge = true
+}
+
+
+resource "aws_s3_bucket_notification" "ccc_maciefindings_bucket_notification" {
+  bucket      = module.ccc_maciefindings_bucket.s3_bucket_id
+  eventbridge = true
+}
+
+
+resource "aws_s3_bucket_notification" "ccc_piimetadata_bucket_notification" {
+  bucket      = module.ccc_piimetadata_bucket.s3_bucket_id
+  eventbridge = true
+}
+
+
+resource "aws_s3_bucket_notification" "ccc_athenaresults_bucket_notification" {
+  bucket      = module.ccc_athenaresults_bucket.s3_bucket_id
+  eventbridge = true
+}
