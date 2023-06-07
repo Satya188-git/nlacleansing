7e11620a65eec9dd22b5fe233b14b4d32b010d46
Saha <koushik.saha@accenture.com>
2023-06-07 16:43:39 +0530

    added try-catch

--- backend/python/src/ccc_audio_copy_lambda/run.py
+++ backend/python/src/ccc_audio_copy_lambda/run.py
@@ -7,14 +7,12 @@ from dateutil import tz
 
 s3 = boto3.client("s3")
 logger = logging.getLogger()
-
 audio_bucket = os.environ['AUDIO_BUCKET']
 call_recordings_bucket = os.environ['CALL_RECORDINGS_BUCKET']
 edix_audio_dir = os.environ['EDIX_AUDIO_DIR']
 
 if os.environ['DEBUG'] == 'enabled':
     logger.setLevel(logging.DEBUG)
-
 else:
     if os.environ['ENV'] == 'prd':
         logger.setLevel(logging.ERROR)
@@ -24,7 +22,19 @@ else:
         # for dev
         logger.setLevel(logging.INFO)
 
-
+def lambda_handler(event, context):
+    logger.info("Event: " + json.dumps(event))
+try:
+    audio_files = get_recent_files(call_recordings_bucket, edix_audio_dir)
+    for fileObjectKey in audio_files:
+        copy_file(call_recordings_bucket, fileObjectKey, audio_bucket, fileObjectKey)
+except Exception as e:
+    errorMessage = str(e)
+    logger.error(errorMessage)
+    return {
+        'errorMessage': errorMessage
+    } 
+    
 def get_recent_files(bucket_name, folder_prefix):
     # Calculate the timestamp for x minutes ago
     minutes_ago = datetime.now(tz.UTC) - timedelta(minutes=5)
@@ -43,7 +53,6 @@ def get_recent_files(bucket_name, folder_prefix):
     logger.info(recent_files)
     return recent_files
 
-
 def copy_file(source_bucket, source_key, destination_bucket, destination_key):
     copy_source = {
         'Bucket': source_bucket,
@@ -54,9 +63,4 @@ def copy_file(source_bucket, source_key, destination_bucket, destination_key):
     logger.info(f"File copied from '{source_bucket}/{source_key}' to '{destination_bucket}/{destFileObjectKey}'.")
 
 
-def lambda_handler(event, context):
-    logger.info("Event: " + json.dumps(event))
-
-    audio_files = get_recent_files(call_recordings_bucket, edix_audio_dir)
-    for fileObjectKey in audio_files:
-        copy_file(call_recordings_bucket, fileObjectKey, audio_bucket, fileObjectKey)
+       
