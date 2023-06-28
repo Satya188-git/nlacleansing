dd9e496cd87665f31cbf6b9ee48bb4ffaf7e802a
Saha <koushik.saha@accenture.com>
2023-06-28 15:57:05 +0530

    Notification for new buckets enabled

--- terraform/pii_cleansing/modules/s3/main.tf
+++ terraform/pii_cleansing/modules/s3/main.tf
@@ -612,6 +612,17 @@ resource "aws_s3_bucket_notification" "ccc_athenaresults_bucket_notification" {
   eventbridge = true
 }
 
+resource "aws_s3_bucket_notification" "ccc_insights_audio_bucket_notification" {
+  bucket      = module.ccc_insights_audio_bucket.s3_bucket_id
+  eventbridge = true
+}
+
+
+resource "aws_s3_bucket_notification" "ccc_callrecordings_bucket_notification" {
+  bucket      = module.ccc_callrecordings_bucket.s3_bucket_id
+  eventbridge = true
+}
+
 
 resource "aws_s3_object" "edix_audio_prefix" {
   key        = "EDIX_AUDIO/"
