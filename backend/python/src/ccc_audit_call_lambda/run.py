c25d4b559e290964512f01deafbdf16b22a7cf29
Saha <koushik.saha@accenture.com>
2023-11-01 14:33:17 +0530

    revised to use new audit table sturcture, corrected to use calltype for metadata file audit

--- backend/python/src/ccc_audit_call_lambda/run.py
+++ backend/python/src/ccc_audit_call_lambda/run.py
@@ -1,6 +1,5 @@
 import os
 import boto3
-import uuid
 import status_list
 import buckets
 import folders
@@ -9,72 +8,72 @@ import json
 from datetime import datetime
 
 s3 = boto3.resource("s3")
+
+debug = os.environ['DEBUG']
+
 logger = logging.getLogger()
-
-if os.environ['DEBUG'] == 'enabled':
+if (debug == 'enabled'):
     logger.setLevel(logging.DEBUG)
-
 else:
-    if os.environ['ENV'] == 'prd':
-        logger.setLevel(logging.ERROR)
-    elif os.environ['ENV'] == 'qa':
-        logger.setLevel(logging.WARNING)
-    else:
-        # for dev
-        logger.setLevel(logging.INFO)
-
+    logger.setLevel(logging.INFO)
 
 def lambda_handler(event, context):
     logger.info("Event: " + json.dumps(event))
-
+    
+    # Read the calltypes JSON file
+    with open('calltypes.json', 'r') as file:
+        calltype_data = json.load(file)
+    
     # Get bucket and file name details
     bucket = event['detail']['bucket']['name']
     key = event['detail']['object']['key']
 
-    logger.info("Bucket: " + bucket)
+    logger.debug("Bucket: " + bucket)
 
     folder = os.path.dirname(key)
+    logger.debug(folder)
     file = os.path.basename(key)
+    logger.debug(file)
     file_ext = os.path.splitext(file)[1]
-
+    logger.debug(file_ext)
 
     if folder:
         path = bucket + '/' + folder + '/'
     else:
         path = bucket
-
     # Create a DynamoDB client
     dynamodb = boto3.resource("dynamodb")
     table_name = os.environ["TABLE_NAME"]
     table = dynamodb.Table(table_name)
 
-    # Create random UUID
-    id = str(uuid.uuid4())
-
     split_file = file.split('_')
     if file_ext not in ('.wav', '.txt', '.json'):
         if len(split_file) > 1:
-            # handling a typical metadata file
+            # handling a typical metadata CSV file
             split_file = file.split('_')
             call_type = split_file[1]
-            file_id = "0"
+            logger.debug("call_type : " + call_type)
+            call_type_number = calltype_data[call_type]
+            logger.debug(call_type_number)
+            file_id = 0
         else:
-            # handling a non-typical metadata file
-            call_type = "0"
-            file_id = "0"            
+            # handling a non-typical metadata CSV file
+            call_type = 0
+            file_id = 0            
     else:
         # handling a typical NLA file
         if len(split_file) > 1:
-            call_type = split_file[1]
+            call_type_number = int(split_file[1])
+            logger.debug(call_type_number)
             file_id = split_file[2]
         else:
             # handling a non-typical NLA file
-            call_type = "0"
-            file_id = "0"
+            call_type = 0
+            file_id = 0
 
     status = ""
 
-    logger.info("Path: " + path)
+    logger.info("Path : " + path)
 
     if path == buckets.UNREFINED:
         status = status_list.AUDIO_FILE_RECEIVED
@@ -100,7 +99,7 @@ def lambda_handler(event, context):
         status = status_list.AUDIO_RECORDINGS_FILE_UPLOADED
     elif path == buckets.RECORDINGS + "/" + folders.EDIX_METADATA + "/":
         status = status_list.METADATA_RECORDINGS_FILE_UPLOADED
-    elif path == buckets.METADATA:
+    elif path == buckets.METADATA + "/" + folders.EDIX_METADATA + "/":
         status = status_list.METADATA_FILE_UPLOADED
     elif path == buckets.AUDIO:
         status = status_list.AUDIO_FILE_UPLOADED
@@ -108,14 +107,11 @@ def lambda_handler(event, context):
         logger.info("Not a valid bucket to audit")
         return
     
-    now = datetime.now()
-    # dd/mm/YY H:M:S
-    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-    
-    logger.info("Auditing status -" + status + " for file - " + file + " at  - " + dt_string)
+    now = datetime.now() 
+    dt_number=int(now.strftime("%Y%m%d%H%M%S%f"))
 
     response = table.put_item(
-        Item={"UUID": id, "CallType": call_type, "FileID": file_id, "Path": path, "FileName": file, "Status": status,
-              "DateTimeStamp": dt_string})
-
-    logger.info(response)
+        Item={"callType": call_type_number, "callId": int(file_id), "path": path, "fileName": file, "status": status,"datetimeStamp": dt_number})
+    logger.debug(response)
+    
+    logger.info("Audited status - " + status + " - for file - " + file + " - at - " + str(dt_number))    
