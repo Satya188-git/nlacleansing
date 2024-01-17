1555ea93deab6380bc24a3e530bed09b1ffd9810
Saha <koushik.saha@accenture.com>
2024-01-17 15:14:15 +0530

    corrected proper topic name for supervisor data notification

--- terraform/pii_cleansing/modules/sns/outputs.tf
+++ terraform/pii_cleansing/modules/sns/outputs.tf
@@ -4,6 +4,6 @@ output "sns-topic-arn" {
 }
 
 output "sns-supervisor-data-notifications-topic-arn" {
-  value       = module.sns.email_subscription_arn
-  description = "ARN for SNS topic"
+  value       = module.supervisor-data-notifications-sns.email_subscription_arn
+  description = "ARN for supervisor-data-notifications-sns topic"
 }
