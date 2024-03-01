52527a35aeb9e89600844f3b2074ae50ab5450d3
Saha <koushik.saha@accenture.com>
2024-03-01 21:26:33 +0530

    tfv10 and tagging changes - draft 16 commentting out sns subscription

--- terraform/pii_cleansing/modules/sns/main.tf
+++ terraform/pii_cleansing/modules/sns/main.tf
@@ -70,9 +70,9 @@ module "supervisor-data-notifications-sns" {
   )
 }
 
-resource "aws_sns_topic_subscription" "supervisor_data_notifications_email_subscription" {
-  depends_on = [ module.supervisor-data-notifications-sns.sns_topic_arn ]
-  topic_arn = "arn:aws:sns:${var.region}:${var.account_id}:${var.company_code}-${var.application_code}-${var.environment_code}-${var.region_code}-sns-nla-supervisor-data-notifications-topic"
-  protocol  = "email"
-  endpoint  = var.supervisordatanotificationemail
-}
+#resource "aws_sns_topic_subscription" "supervisor_data_notifications_email_subscription" {
+#  depends_on = [ module.supervisor-data-notifications-sns.sns_topic_arn ]
+#  topic_arn = "arn:aws:sns:${var.region}:${var.account_id}:${var.company_code}-${var.application_code}-${var.environment_code}-${var.region_code}-sns-nla-supervisor-data-notifications-topic"
+#  protocol  = "email"
+#  endpoint  = var.supervisordatanotificationemail
+#}
