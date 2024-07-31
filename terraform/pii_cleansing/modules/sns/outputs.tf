54a2002f9251228278fcd1fe610ccaec2a9477e2
Somesh Shirsath <SShirsat@sdgecontractor.com>
2024-07-31 13:41:34 +0530

    resolving sns output2

--- terraform/pii_cleansing/modules/sns/outputs.tf
+++ terraform/pii_cleansing/modules/sns/outputs.tf
@@ -9,7 +9,7 @@ output "sns-supervisor-data-notification-topic-subscription-arn" {
 }
 
 output "access-denied-topic-arn" {
-  value       = module.access-denied-notification-sns.email_subscription_arn
+  value       = module.access_denied_notification_sns.email_subscription_arn
   description = "ARN for access-denied-ana topic's email subscription"
 }
 
