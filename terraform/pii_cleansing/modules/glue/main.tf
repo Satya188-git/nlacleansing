5f1eeb7301c8997d5957884563b7271b54085b4f
Somesh Shirsath <SShirsat@sdgecontractor.com>
2024-11-22 18:58:56 +0530

    PII change for historical calls-6

--- terraform/pii_cleansing/modules/glue/main.tf
+++ terraform/pii_cleansing/modules/glue/main.tf
@@ -331,13 +331,13 @@ module "historicals_calls_etl_job" {
       name                         = "historicals_calls_etl_job"
       glue_job_description         = "This is a etl job to send all historical data into sqs for batching."
       glue_job_version             = "4.0"
-      # glue_command_job_name        = "glueetl"
+      glue_command_job_name        = ""
       glue_command_python_version  = 3
       glue_command_script_location = "s3://sdge-dtdes-dev-wus2-s3-nla-historical-calls/ETL_SCRIPTS/run.py"
       max_concurrent_runs          = 1
       max_retries                  = 0
-      # connections                  = [join("-", [local.standard_name, "connection2"])]
-      # default_arguments            = { "--job-language" = "python" }
+      connections                  = {}
+      default_arguments            = {}
       job_resources = {
         "number_of_workers" = 10
         "worker_type"       = "G.1X"
