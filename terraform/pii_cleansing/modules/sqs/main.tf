ef9dce5ce5065a15245ea663df2ea40058a50cc9
Somesh Shirsath <SShirsat@sdgecontractor.com>
2024-11-22 18:42:19 +0530

    PII change for historical calls-4

--- terraform/pii_cleansing/modules/sqs/main.tf
+++ terraform/pii_cleansing/modules/sqs/main.tf
@@ -8,14 +8,14 @@ locals {
   region_code      = var.region_code
   owner            = var.owner
   tags = {
-    "sempra:gov:tag-version" = var.tag-version 	# tag-version         = var.tag-version
+    "sempra:gov:tag-version" = var.tag-version  # tag-version         = var.tag-version
     billing-guid        = var.billing-guid
-    "sempra:gov:unit"   = var.unit 				# unit                = var.unit
     portfolio           = var.portfolio
     support-group       = var.support-group
     "sempra:gov:environment" = var.environment 	# environment         = var.environment
     "sempra:gov:cmdb-ci-id"  = var.cmdb-ci-id 	# cmdb-ci-id          = var.cmdb-ci-id
     data-classification = var.data-classification
+	"sempra:gov:unit"   = var.unit 				# unit                = var.unit
   }
 }
 
