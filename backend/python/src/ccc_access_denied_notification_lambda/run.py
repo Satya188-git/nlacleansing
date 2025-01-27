9cc4024dd19d2992615c98d4d2ee77471e584bd7
Srivastava <sanket.a.srivastava@accenture.com>
2025-01-27 18:21:50 +0530

    access denied code update

--- backend/python/src/ccc_access_denied_notification_lambda/run.py
+++ backend/python/src/ccc_access_denied_notification_lambda/run.py
@@ -1,6 +1,7 @@
 import logging
 import boto3
 import os
+import re
 
 sns_topic_arn = os.environ["SNS_TOPIC_ARN"]
 logger = logging.getLogger()
@@ -25,24 +26,39 @@ def lambda_handler(event, context):
 
 def check_access_denied(content, bucket, key):
     lines = content.splitlines()
-    access_denied = False
+    access_denied_user = False
+    access_denied_service_role = False
     for line in lines:
+        logger.info(f"Iterating Lines: {line}")
         if ("REST.GET.OBJECT" in line) and ("AccessDenied" in line):
-            access_denied = True
-            break
-    if access_denied:
-        message = "Access Denied detected in : " + bucket + "/" + key
+            if("AWSReservedSSO" in line):
+                email_pattern = r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}'
+                emails = re.findall(email_pattern, str(line))
+                access_denied_user = True
+                break
+            if("AWSServiceRoleFor" in line):
+                access_denied_service_role = True
+                break
+    if access_denied_user:
+        User_email_string = ', '.join(emails)
+        message = "Unauthorized Access Detected For User: " + User_email_string + " on " + bucket + "/" + key
+        subject = "NLA : S3 Unauthorized Access Alert For User"
         logger.info(message)
-        send_sns_notification(message)
+        send_sns_notification(message, subject)
+    if access_denied_service_role:
+        message = "Access Denied detected for Service Role : " + bucket + "/" + key
+        subject = "NLA : S3 Access Denied Alert For Service Role"
+        logger.info(message)
+        send_sns_notification(message, subject)
 
 # defination of sns and publish notification
-def send_sns_notification(message):
+def send_sns_notification(message, subject):
     sns_client = boto3.client("sns")
     sns_topic = sns_topic_arn
     logger.info(f"sns topic: {sns_topic}")
     try:
         response = sns_client.publish(
-            TopicArn=sns_topic, Message=message, Subject="NLA : S3 Access Denied Alert."
+            TopicArn=sns_topic, Message=message, Subject=subject
         )
     except Exception as e:
         logger.info("error in publishing msg")
